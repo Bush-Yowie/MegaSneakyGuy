1c30a6cbc532ebd98f305e4369368e3df6814b5f
Holyvale <92715674+Holyvale@users.noreply.github.com>
2022-09-06 19:34:59 +1000

    Added Movement

---
+++ Assets/Scripts/Player/InputSettings/PlayerController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    //Input Fields
+    private PlayerContoller playerActionsAsset;
+    private InputAction move;
+
+    //movement fields
+    private Rigidbody rb;
+    [SerializeField]
+    private float movementForce = 1f;
+    [SerializeField]
+    private float maxspeed = 5f;
+    private Vector3 forceDirection = Vector3.zero;
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        playerActionsAsset = new PlayerContoller();
+
+
+    }
+
+
+    private void OnEnable()
+    {
+        move = playerActionsAsset.Player.Move;
+        playerActionsAsset.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        playerActionsAsset.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * movementForce;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * movementForce;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        if (rb.velocity.y < 0f)
+            rb.velocity += Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
+
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.y = 0;
+        if (horizontalVelocity.sqrMagnitude > maxspeed * maxspeed)
+            rb.velocity = horizontalVelocity.normalized * maxspeed + Vector3.up * rb.velocity.y;
+    }
+
+    private Vector3 GetCameraForward(Camera playerCamera)
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+
+    private Vector3 GetCameraRight(Camera playerCamera)
+    {
+        Vector3 right = playerCamera.transform.right;
+        right.y = 0;
+        return right.normalized;
+    }
+
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(this.transform.position + Vector3.up * 0.25f, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, 0.3f))
+            return true;
+        else
+            return false;
+    }
+
+}
