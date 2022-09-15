c5f9ff5bf524a71c5e229b115b09d31502f1188b
Bush-Yowie <jaedongamer@gmail.com>
2022-09-15 21:05:31 +1000

    Added Proper AI Detection + Cleaned Up Unused code

--- Assets/Scripts/AI/Basic Components/EnemyAI.cs
+++ Assets/Scripts/AI/Basic Components/EnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI: MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     [Header("Movement + Patrolling")]
     [Tooltip("Patrol Point Wait Time")]
@@ -42,25 +42,35 @@ public class EnemyAI: MonoBehaviour
     public bool canSeePlayer;
     [Tooltip("Player Detected")]
     public bool playerDetected;
+    [Tooltip("Player Partially Detected")]
+    public bool playerPartDetected;
 
     Vector3 target;
     float waitTimer;
+    float detectionTimer;
     bool movingOn;
-    
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
 
         waitTimer = patrolPointWait;
+        detectionTimer = detectDelay;
         movingOn = false;
-        headPos = transform.position + new Vector3 (0, headOffset, 0);
+        headPos = transform.position + new Vector3(0, headOffset, 0);
 
-        if(!ignorePatrol)
+        //AI does not start patrolling when game is started without sending them to a point first, could maybe fix this by spawning them on a patrol point but this makes sure it always starts
+        if (!ignorePatrol)
         {
             GoToPatrolPoint();
         }
 
+        //lets the AI see
         StartCoroutine(VisionRoutine());
     }
 
@@ -71,11 +81,25 @@ public class EnemyAI: MonoBehaviour
 
     private void Update()
     {
+        Detection();
+
+        if (playerDetected && canSeePlayer)
+        {
+            //combat code
+        }
+        else if (playerDetected && !canSeePlayer)
+        {
+            //have another countdown then go to hunting mode
+        }
+        else if (playerPartDetected)
+        {
+            //investigation code
+        }
+        //AI will move to set points in the order they are listed in patrol points list variable, staying at each point for a set time before moving to the next one
         if (!ignorePatrol)
         {
             Patrolling();
         }
-
     }
 
     private void MoveTo()
@@ -85,15 +109,20 @@ public class EnemyAI: MonoBehaviour
 
     private void StopMoving()
     {
+        //sets destination to its on position
         agent.SetDestination(transform.position);
     }
 
     private void Patrolling()
     {
+        //when ai gets to patrol point, start waiting
+        if (Vector3.Distance(transform.position, target) < 1 && !movingOn)
+        {
+            waitTimer -= Time.deltaTime;
+        }
         //once ai has waited x time, go to next patrol point
         if (Vector3.Distance(transform.position, target) < 1 && waitTimer <= 0)
         {
-            //Debug.Log("AI wait count down has reached 0 or below, moving to next patrol point");
             movingOn = true;
             NextPatrolPoint();
             GoToPatrolPoint();
@@ -101,25 +130,19 @@ public class EnemyAI: MonoBehaviour
         //once ai is not at patrol point, reset wait timer
         if (Vector3.Distance(transform.position, target) > 1 && movingOn)
         {
-            //Debug.Log("AI has left previous patrol point, reseting timer");
             movingOn = false;
             waitTimer = patrolPointWait;
         }
-        //when ai gets to patrol point, start waiting
-        if (Vector3.Distance(transform.position, target) < 1 && !movingOn)
-        {
-            //Debug.Log("AI has reached patrol point, starting count down");
-            waitTimer -= Time.deltaTime;
-            //Debug.Log("Wait timer is currently at: " + waitTimer);
-        }
     }
 
+    //sends AI to next patrol point
     private void GoToPatrolPoint()
     {
         target = patrolPoints[patrolPointIndex].position;
         MoveTo();
     }
 
+    //sets the next patrol point that the AI will move to, loops around to 0 when reaching the last point in the list
     private void NextPatrolPoint()
     {
         patrolPointIndex++;
@@ -130,11 +153,12 @@ public class EnemyAI: MonoBehaviour
         }
     }
 
+    //runs infinitely with a 0.2 second delay between each repeat as to not crash the game/computer
     private IEnumerator VisionRoutine()
     {
         WaitForSeconds wait = new WaitForSeconds(visDelay);
 
-        while(true)
+        while (true)
         {
             yield return wait;
 
@@ -147,9 +171,10 @@ public class EnemyAI: MonoBehaviour
         //Checks if player is in vision range
         Collider[] rangeChecks = Physics.OverlapSphere(headPos, visRange, tarMask);
 
-        //Only runs if valid object is in vision range (In this case its the player)
+        //Only runs if player is in range
         if (rangeChecks.Length != 0)
         {
+            //gets a reference to player position
             Transform visTarget = rangeChecks[0].transform;
             Vector3 directionToVisTarget = (visTarget.position - transform.position).normalized;
 
@@ -158,6 +183,7 @@ public class EnemyAI: MonoBehaviour
             {
                 float distanceToVisTarget = Vector3.Distance(headPos, visTarget.position);
 
+                //If player is in range, is in correct angle range and is not obstructed
                 if (!Physics.Raycast(headPos, directionToVisTarget, distanceToVisTarget, obMask))
                 {
                     canSeePlayer = true;
@@ -172,32 +198,45 @@ public class EnemyAI: MonoBehaviour
                 canSeePlayer = false;
             }
         }
+        //sets can see player to false when player runs out of range after being spotted
         else if (canSeePlayer)
         {
             canSeePlayer = false;
         }
     }
 
-    private void CombatChasing()
+    private void Detection()
     {
-    //    agent.SetDestination(player.position);
-    }
-
-    private void CombatShooting()
-    {
-     //   agent.SetDestination(transform.position);
-
-     //   transform.LookAt(player);
-
-        //  if (!hasAttacked)
+        //while player in AI fov and not fully detected tick down timer
+        if (canSeePlayer && !playerDetected)
         {
-            //attack code goes here
+            detectionTimer -= Time.deltaTime;
+            Debug.Log("Player is in an AI's fov, counting down");
+        }
+        //if player leaves AI fov but timer has ticked down a reasonable amount, AI will investigate
+        if (detectionTimer <= detectDelay - visDelay * 2 && !canSeePlayer && !playerDetected)
+        {
+            Debug.Log("Player has been in an AI's fov for long enough to make the AI investigate");
+            playerPartDetected = true;
 
-            //        hasAttacked = true;
-            //       Invoke(nameof(ResetAttack), attackCooldown);
+            //reset timer when partially spotted
+            detectionTimer = detectDelay;
+        }
+        //if player in AI fov for full timer, go to combat state, ignore investigation state
+        if (detectionTimer <= 0 && !playerDetected)
+        {
+            Debug.Log("Player has been in an AI's fov long enough to initiate combat");
+            playerDetected = true;
+            playerPartDetected = false;
+
+            //reset timer when player is spotted
+            detectionTimer = detectDelay;
+        }
+        //reset timer if timer player was in an AI's fov for long enough to trigger the timer but not long enough to trigger being spotted partially or fully (Imagine like coyote time for staying hidden) 
+        if (detectionTimer < detectDelay && !playerDetected && !playerPartDetected && !canSeePlayer)
+        {
+            detectionTimer = detectDelay;
         }
     }
-
-    
 }
 
